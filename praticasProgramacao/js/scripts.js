83c124e9842640c9718b53933c30b281018af870
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-04-04 09:24:21 -0300

    UpDate20230403

---
+++ praticasProgramacao/js/scripts.js
@@ -0,0 +1,47 @@
+/*
+// string
+// cont nome = prompt("Por Favor, Digite um nome")
+
+const n1 = 10
+const n2 = 7
+const aprovado = true
+const minimo = 9
+
+// Operadores 
+
+console.log('Operadores Aritiméticos')
+console.log( n1 + n2 )
+console.log( n1 - n2 )
+console.log( n1 * n2 )
+console.log( n1 / n2 )
+console.log( n1 % n2 )
+
+console.log('Operadores Relacionais')
+console.log( n1 == n2 )
+console.log( n2 != n2 )
+console.log( n1 > n2 )
+console.log( n1 < n2 )
+console.log( n1 >= n2)
+console.log( n1 <= n2 )
+
+console.log('Operadores Logicos')
+console.log(aprovado)
+console.log(!apravado)
+console.log( n2 >= 7 && n2 >= minimo)
+console.log( n2 >= 7 || n2 >= minimo)
+
+*/
+
+const n1 = prompt('Digite o numero 1')
+const n2 = pronpt('Digite o numero 2')
+
+var x = true && true;
+
+var y = true && false;
+
+var z = !false;
+
+var w = false || true; c
+
+console.log(x, y, z, w);
+
